bca12a53b21f4c7f26d85eb5d411b59a4b7a7bfa
Cristina Sisu <cristina.sisu@gmail.com>
2011-07-12 13:19:58 -0400

    hello one more time

--- src/test.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on 12 Jul 2011
-
-@author: cristinasisu
-'''
-print "hello world"
---
+++ src/test/helloworld.py
@@ -0,0 +1,8 @@
+'''
+Created on 12 Jul 2011
+
+@author: CristinaSisu
+'''
+
+print "hello"
+print "hello twice"
